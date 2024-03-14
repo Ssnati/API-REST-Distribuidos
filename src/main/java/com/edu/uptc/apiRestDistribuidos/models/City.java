83ec631ee638f43ea1c640b29a2356d7a65c15a3
SebasR-25 <seruprisatur2703@gmail.com>
2024-03-14 18:53:37 -0500

    Modelos Person y City
    Se agrega los modelos de persona y ciudad, además se agrega la dependencia de Lombok al pom del proyecto

--- src/main/java/com/edu/uptc/apiRestDistribuidos/models/City.java
+++ src/main/java/com/edu/uptc/apiRestDistribuidos/models/City.java
@@ -1,8 +1,14 @@
 package com.edu.uptc.apiRestDistribuidos.models;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 
 @Entity
 public class City {
-    
+    private int id;
+    private String nombreCiudad;    
 }
