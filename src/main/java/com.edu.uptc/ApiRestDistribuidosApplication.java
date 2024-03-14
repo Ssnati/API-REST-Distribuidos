aa1db8f3da7ac3083c179a2a9ebccceb688843f7
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-03-14 17:13:28 -0500

    First commit

---
+++ src/main/java/com.edu.uptc/ApiRestDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uptc.apirestdistribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDistribuidosApplication.class, args);
+	}
+
+}
