f0b60934cd5f8bae52f4124d33d7a5d9e8de669c
SebasR-25 <seruprisatur2703@gmail.com>
2024-03-14 19:09:06 -0500

    Adición de notaciones
    Se agregan las notaciones  a todos los atributos de persona y ciudad, además de agregar la notación de tablas a las  clases

--- src/main/java/com/edu/uptc/apiRestDistribuidos/models/City.java
+++ src/main/java/com/edu/uptc/apiRestDistribuidos/models/City.java
@@ -1,6 +1,11 @@
 package com.edu.uptc.apiRestDistribuidos.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +13,12 @@ import lombok.Setter;
 @Getter
 
 @Entity
+@Table (name = "CIUDADES")
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ciudad")
     private int id;
+    @Column (name = "nombre_ciudad")
     private String nombreCiudad;    
 }
