16bdec241d37100352145645f32af88ced018770
Ssolaris <108376306+Ssnati@users.noreply.github.com>
2024-03-19 15:31:07 -0500

    Properties out of source code

--- src/main/java/com/edu/uptc/apiRestDistribuidos/ApiRestDistribuidosApplication.java
+++ src/main/java/com/edu/uptc/apiRestDistribuidos/ApiRestDistribuidosApplication.java
@@ -1,30 +1,12 @@
 package com.edu.uptc.apiRestDistribuidos;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiRestDistribuidosApplication {
 
-    public static void main(String[] args) throws IOException {
-        InputStream fr = new FileInputStream("src/main/resources/application.properties");
-        if(args.length>0){
-            Properties p = new Properties();
-            p.load(fr);
-            String ipPort = args[0];
-            p.setProperty("spring.datasource.url", "dbc:mysql://"+ipPort+"/DISTRIBUIDOS");
-            fr.close();
-
-            FileOutputStream fos = new FileOutputStream("src/main/resources/application.properties");
-            p.store(fos, "");
-        }
+    public static void main(String[] args) {
         SpringApplication.run(ApiRestDistribuidosApplication.class, args);
     }
 
